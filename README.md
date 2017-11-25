4efce35496ed33dae3ee451ef88472373eb3d1bf
Mateusz <kacpura.mateusz@protonmail.com>
2017-11-25 01:35:07 +0100

    do przodu

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cvraport
+
+//Tutaj zapisuje ostatnie plany i zmiany dokonywane nad projektem//
+//1. W tej chwili staram się stworzyć sensowny układ katalogów//
+
--- main.c
+++ main.c
@@ -1,5 +1,29 @@
-#<ncurses.h>
-
-int mainc(){
-	
+#include <ncurses.h>  // ncurses.h zawiera stdio.h
+#include <string.h>
+ 
+int main()
+{
+    char mesg[]="Podaj napis: "; // wiadomość do wyświetlenia
+    char str[80];
+ 
+    int row,col;                 // zmienne do przechowywania
+                                 // ilości kolumn i rzędów
+ 
+    initscr();                   // tryb curses
+ 
+    getmaxyx(stdscr,row,col);    // pozyskiwanie ilości
+                                 // kolumn i rzędów
+ 
+    mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
+                                 // drukowanie wiadomości
+                                 // na środku ekranu
+ 
+    getstr(str);
+ 
+    mvprintw(LINES - 2, 0, "Podales: %s", str);
+ 
+    getch();
+    endwin();
+ 
+    return 0;
 }
